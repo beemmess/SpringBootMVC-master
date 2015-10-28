a6c3a7782fb573fcc52ac9bd71b170baec6c5a2f
audurgudjons <audurgudjons8@gmail.com>
2015-10-28 15:32:32 +0000

    eventData klasi
    klasi

---
+++ src/main/java/project/controller/EventData.java
@@ -0,0 +1,12 @@
+package project.controller;
+
+/**
+ * Created by audurgudjons on 28/10/15.
+ */
+public class EventData {
+    public String eventType;
+
+    public EventData(String atburdur){
+        eventType = atburdur;
+    }
+}
