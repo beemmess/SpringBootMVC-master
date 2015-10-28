0648151d392722623ed09a413353c375abb69182
hraldur <hrj28@hi.is>
2015-10-28 19:31:32 +0000

    Signup
    prófa að búa til signup sem tengist db

---
+++ src/main/java/project/persistence/entities/User/SignUp.java
@@ -0,0 +1,47 @@
+package project.persistence.entities.User;
+
+import javax.persistence.*;
+
+/**
+ * Created by Hrafnhildur on 10/28/2015.
+ */
+@Entity
+@Table(name = "signUp")
+public class SignUp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String userName;
+
+
+    private String email;
+    private String password;
+
+
+    public SignUp() {
+    }
+
+    public SignUp(String userName, String email, String password) {
+        this.userName = userName;
+
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUserName() {return userName; }
+
+    public void setUserName(String userName) {this.userName = userName;}
+
+    public String getEmail() { return email;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public void setPassword(String password) {this.password = password;}
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Sign up [User Name=%s, email=%s, password=%s]",
+                userName,email,password);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/User/signUp.jsp
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="sf" uri="http://www.springframework.org/tags/form" %>
+
+<html lang="en">
+    <head>
+
+    </head>
+    <body>
+        <h1>Sign Up</h1>
+    
+
+
+    </body>
+</html>
