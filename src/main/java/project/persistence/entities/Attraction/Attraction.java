71aae345e445af22afac35d8321cd779caeb39c6
beemmess <bms4@hi.is>
2015-10-28 16:16:34 +0000

    fleiri klasar
    klasar

---
+++ src/main/java/project/persistence/entities/Attraction/Attraction.java
@@ -0,0 +1,11 @@
+package project.persistence.entities.Attraction;
+
+/**
+ * Created by bjarkimar23 on 28.10.2015.
+ */
+public class Attraction {
+
+    public String type;
+    public String name;
+
+}
---
+++ src/main/java/project/persistence/entities/Attraction/Restaurant.java
@@ -0,0 +1,9 @@
+package project.persistence.entities.Attraction;
+
+/**
+ * Created by bjarkimar23 on 28.10.2015.
+ */
+public class Restaurant extends Attraction {
+
+    
+}
