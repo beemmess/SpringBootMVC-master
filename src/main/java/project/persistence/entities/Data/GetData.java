674a6d3785f2c009b80673f518d07a65fa72fcfc
beemmess <bms4@hi.is>
2015-10-28 16:53:22 +0000

    eitthvad
    eitthvad

---
+++ src/main/java/project/persistence/entities/Data/GetData.java
@@ -0,0 +1,7 @@
+package project.persistence.entities.Data;
+
+/**
+ * Created by bjarkimar23 on 28.10.2015.
+ */
+public class GetData {
+}
---
+++ src/main/java/project/persistence/entities/User/User.java
@@ -0,0 +1,10 @@
+package project.persistence.entities.User;
+
+/**
+ * Created by bjarkimar23 on 28.10.2015.
+ */
+public class User {
+    public String userName;
+    public String email;
+    public String favourites;
+}
