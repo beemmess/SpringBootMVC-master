19a1105af8ae5ffaad4108bddb31582e6c03e2a0
hraldur <hrj28@hi.is>
2015-11-03 22:40:27 +0000

    signup entity
    signup entity

--- src/main/java/project/persistence/entities/User/SignUp.java
+++ src/main/java/project/persistence/entities/User/SignUp.java
@@ -8,40 +8,25 @@ import javax.persistence.*;
 @Entity
 @Table(name = "signUp")
 public class SignUp {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
     private String userName;
-
-
     private String email;
     private String password;
-
 
     public SignUp() {
     }
 
     public SignUp(String userName, String email, String password) {
         this.userName = userName;
-
         this.email = email;
         this.password = password;
     }
 
-    public String getUserName() {return userName; }
+    
 
-    public void setUserName(String userName) {this.userName = userName;}
 
-    public String getEmail() { return email;}
 
-    public void setEmail(String email) {this.email = email;}
-
-    public void setPassword(String password) {this.password = password;}
-
-    //@Override
-    //public String toString() {
-     //   return String.format(
-    //            "Sign up [User Name=%s, email=%s, password=%s]",
-   //             userName,email,password);
- //   }
 }
