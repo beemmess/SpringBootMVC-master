c2e19ca92125c0acf7881bd63fef68afeb2d4e21
hraldur <hrj28@hi.is>
2015-11-24 23:09:13 +0000

    Update SecurityConfig.java

--- src/main/java/project/config/SecurityConfig.java
+++ src/main/java/project/config/SecurityConfig.java
@@ -31,7 +31,7 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/", "/public/**").permitAll()
+                .antMatchers("/", "/public/**","/currency**", "/events/**", "/info/**", "/streetmap/**").permitAll()
                 .antMatchers("/userPage/**").hasAuthority("USER")
                 .anyRequest().fullyAuthenticated()
                 .and()
